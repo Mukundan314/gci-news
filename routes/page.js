374c4a2f253196f7eaa7c57fee4c28e403c521f8
Mukundan Senthil <mukundan314@gmail.com>
2020-01-04 11:34:43 +0530

    Add code to show news

--- routes/page.js
+++ routes/page.js
@@ -1,9 +1,27 @@
-var express = require('express');
+const express = require('express');
+const NewsAPI = require('newsapi');
+
+var newsapi = new NewsAPI('55c1fb1120be45aab761b01d1c00d16d');
 var router = express.Router();
+
+async function getArticles(page) {
+  var response = await newsapi.v2.everything({
+    q: 'linux OR android OR open-source',
+    sources: 'google-news',
+    sortBy: 'publishedAt',
+    page: page,
+  });
+
+  return response.articles;
+}
 
 /* GET home page. */
 router.get('/:page', function(req, res, next) {
-  res.send(req.params.page)
+  getArticles(req.params.page).then(articles => {
+    res.render('page', { articles })
+  }).catch((err) => {
+    res.send(req.params.page);
+  });
 });
 
 module.exports = router;
